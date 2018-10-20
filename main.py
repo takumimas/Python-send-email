5e11a280796c20960ad8005500c6c7e583be2eba
増田拓海 <masudatakumi@masudatakuminoMacBook-puro.local>
2018-10-20 21:24:06 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import smtplib
+from email.mime.text import MIMEText
+def send_email(body,subject,from_add,to_add,password,smtp_add,port):
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = from_add
+    msg["To"] = to_add
+    s = smtplib.SMTP(smtp_add,port)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(from_add,password)
+    s.send_message(msg)
+    s.close()
